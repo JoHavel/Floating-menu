ccf6a4a7791e1778bd3dbdb49f127c4a1f4f472a
Jonas Havelka <jonas.havelka@moznabude.cz>
2019-08-13 15:37:26 +0200

    Move menu handling code from application to view.

--- app/src/main/java/cz/moznabude/floatingcontrol/MainActivity.kt
+++ app/src/main/java/cz/moznabude/floatingcontrol/MainActivity.kt
@@ -1,89 +1,18 @@
 package cz.moznabude.floatingcontrol
 
 import android.os.Bundle
-import android.os.Handler
-import android.view.MotionEvent
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.abs
-import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
 
-    private val handlerOfLongTouch = Handler()
-    private var isActive = false
-    private var isOpened = false
-    private lateinit var menu: MenuView
-    private val runnable = Runnable {
-        Toast.makeText(applicationContext, "pressed once", Toast.LENGTH_SHORT).show()
-        isActive = false
-        println(c.x - f.x)
-        println(c.y - f.y)
-        menu.appear()
-        menu.x = c.x - menu.width/2
-        menu.y = c.y - menu.height/2
-        isOpened = true
-    }
-
-    private val f = Coordinates(0f, 0f)
-    private val c = Coordinates(0f, 0f)
-    //private lateinit var screenSize: Coordinates
+    private lateinit var menuView: MenuView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        menu = floating_menu
-        menu.x = -Float.MAX_VALUE
-        menu.y = -Float.MAX_VALUE
-
-        //screenSize = Coordinates(screen.width.toFloat(), screen.height.toFloat())
-
-        screen.setOnTouchListener { _, event ->
-            if (event.actionMasked == MotionEvent.ACTION_DOWN) {
-                isActive = true
-                handlerOfLongTouch.postDelayed(runnable, 1000)
-                f.x = event.x
-                f.y = event.y
-            }
-
-            if (isActive) {
-                fun stop() {
-                    handlerOfLongTouch.removeCallbacks(runnable)
-                    isActive = false
-                }
-                if (event.actionMasked == MotionEvent.ACTION_MOVE) {
-                    c.x = event.x
-                    c.y = event.y
-                    if (abs(c.x - f.x) > 100 || abs(c.y - f.y) > 100) {
-                        stop()
-                    }
-                }
-                if (event.actionMasked == MotionEvent.ACTION_UP || event.actionMasked == MotionEvent.ACTION_POINTER_DOWN) {
-                    stop()
-                }
-            }
-
-            if (event.actionMasked == MotionEvent.ACTION_UP && isOpened) {
-                menu.disappear()
-                isOpened = false
-                c.x = event.x
-                c.y = event.y
-                val direction = (c.y - f.y) / (c.x - f.x)
-                if(sign(c.x-f.x) > 0) {
-                    when(direction) {
-                        in (-0.5f).rangeTo(0.5f) -> println("next")
-                        else -> println("Error")
-                    }
-                } else {
-
-                }
-            }
-            true
-        }
+        menuView = floating_menu
     }
 
 
 }
-
-private data class Coordinates(var x: Float, var y: Float)
--- app/src/main/java/cz/moznabude/floatingcontrol/MenuView.kt
+++ app/src/main/java/cz/moznabude/floatingcontrol/MenuView.kt
@@ -1,22 +1,97 @@
 package cz.moznabude.floatingcontrol
 
 import android.content.Context
+import android.os.Handler
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.GridLayout
+import kotlin.math.abs
+import kotlin.math.sign
 
 class MenuView: GridLayout {
 
-    init {
-        View.inflate(context, R.layout.menu, this)
+    private var isActive = false
+    private var isOpened = false
+    private val f = Coordinates(0f, 0f)
+    private val c = Coordinates(0f, 0f)
+
+    private val handlerOfLongTouch = Handler()
+    private val runnable = Runnable {
+        isActive = false
+        println(c.x - f.x)
+        println(c.y - f.y)
+        appear()
+        x = c.x - width/2
+        y = c.y - height/2
+        isOpened = true
     }
 
-    fun disappear() { this.visibility = View.GONE }
-    fun appear() { this.visibility = View.VISIBLE }
+    init {
+        View.inflate(context, R.layout.menu, this)
+        x = -Float.MAX_VALUE
+        y = -Float.MAX_VALUE
+    }
 
     constructor(context: Context): super(context)
     constructor(context: Context, attrs: AttributeSet?): super(context, attrs)
-
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
+
+
+    private fun disappear() { this.visibility = View.GONE }
+    private fun appear() { this.visibility = View.VISIBLE }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        setListener()
+    }
+
+    private fun setListener() {
+        (parent as ViewGroup).setOnTouchListener { _, event ->
+            if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+                isActive = true
+                handlerOfLongTouch.postDelayed(runnable, 1000)
+                f.x = event.x
+                f.y = event.y
+            }
+
+            if (isActive) {
+                fun stop() {
+                    handlerOfLongTouch.removeCallbacks(runnable)
+                    isActive = false
+                }
+                if (event.actionMasked == MotionEvent.ACTION_MOVE) {
+                    c.x = event.x
+                    c.y = event.y
+                    if (abs(c.x - f.x) > 100 || abs(c.y - f.y) > 100) {
+                        stop()
+                    }
+                }
+                if (event.actionMasked == MotionEvent.ACTION_UP || event.actionMasked == MotionEvent.ACTION_POINTER_DOWN) {
+                    stop()
+                }
+            }
+
+            if (event.actionMasked == MotionEvent.ACTION_UP && isOpened) {
+                disappear()
+                isOpened = false
+                c.x = event.x
+                c.y = event.y
+                val direction = (c.y - f.y) / (c.x - f.x)
+                if(sign(c.x-f.x) > 0) {
+                    when(direction) {
+                        in (-0.5f).rangeTo(0.5f) -> println("next")
+                        else -> println("Error")
+                    }
+                } else {
+
+                }
+            }
+            true
+        }
+    }
+
+    private data class Coordinates(var x: Float, var y: Float)
 }
